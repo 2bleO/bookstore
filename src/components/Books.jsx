42e5b4152d5de5e367bcaf87b8b7e27800eff446
onesis <Onesis.olea@gmail.com>
2021-09-07 00:25:25 -0400

    Modify Books.js
    Setup booklist state.
    Create Book form

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,11 +1,28 @@
-// import React, { useState } from 'react';
+import { useState } from 'react';
+import Book from './Book';
 
-const Books= () => {
+const Books = () => {
+  const [books] = useState([{
+    id: 0,
+    title: 'Wow',
+    author: 'Me',
+  }]);
 
   return (
-    <div>
+    <>
       <h3>All Books</h3>
-    </div>
+      <ul>
+        { books.map((book) => (
+          <Book key={book.id} props={book} />
+        ))}
+      </ul>
+      <form>
+        <h4>Add New Book</h4>
+        <input placeholder="Title" />
+        <input placeholder="Author" />
+        <button type="button">Add Book</button>
+      </form>
+    </>
   );
 };
 
