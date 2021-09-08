276108f8334ff5500e4bdc29cf37e4f44edcc4a7
onesis <Onesis.olea@gmail.com>
2021-09-08 16:12:40 -0400

    Add Submit and remove book functionality

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable react/prop-types */
-
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = ({ props }) => {
+  const dispatch = useDispatch();
   const { id, title, author } = props;
+
+  const removeBookFromStore = (e) => {
+    dispatch(removeBook({ id: e.target.id }));
+  };
+
   return (
     <li>
       <span>{title}</span>
       <span> by </span>
       <span>{author}</span>
-      <button id={id} type="button">Delete</button>
+      <button id={id} type="button" onClick={removeBookFromStore}>Delete</button>
     </li>
   );
 };
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,27 +3,27 @@ const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
-export const addBook = payload => ({
+export const addBook = (payload) => ({
   type: ADD_BOOK,
-  payload
-})
+  payload,
+});
 
-export const removeBook = payload => ({
+export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
-  payload
-})
+  payload,
+});
 
-const reducer =  (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-      case ADD_BOOK:
-        return [...state, action.payload];
-        
-      case REMOVE_BOOK:
-        return state.filter((book) => book.id !== action.payload.id);
-  
-      default:
-          return state;
-      }
-  };
-  
-  export default reducer;
+    case ADD_BOOK:
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
