51181b51a09695005faa9812a35541e6e71332b3
onesis <Onesis.olea@gmail.com>
2021-09-10 00:00:55 -0400

    Create/ Retrieve appid and fetch books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,28 +1,32 @@
-import { useState, React } from 'react';
+import { useState, useEffect, React } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as UUID } from 'uuid';
-import { addBook } from '../redux/books/books';
+import { addBook, createApp, getBooks } from '../redux/books/books';
 import Book from './Book';
 
 const Books = () => {
   const dispatch = useDispatch();
+  useEffect(() => {
+    createApp();
+    dispatch(getBooks());
+  }, [dispatch]);
   const books = useSelector((state) => state.booksReducer);
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
   const setBookTitle = (e) => {
     setTitle(e.target.value);
   };
 
-  const setBookAuthor = (e) => {
-    setAuthor(e.target.value);
+  const setBookCategory = (e) => {
+    setCategory(e.target.value);
   };
 
   const submitBookToStore = () => {
     const newBook = {
       id: UUID(),
       title,
-      author,
+      category,
     };
     dispatch(addBook(newBook));
   };
@@ -38,7 +42,7 @@ const Books = () => {
           ))}
         </ul>
         <input placeholder="Title" onChange={setBookTitle} />
-        <input placeholder="Author" onChange={setBookAuthor} />
+        <input placeholder="Category" onChange={setBookCategory} />
         <button type="button" onClick={submitBookToStore}>Add Book</button>
       </form>
     </>
